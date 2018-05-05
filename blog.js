04facc370559e773fa898745f65a3d5d72352c79
JComerford <comerford.jac@gmail.com>
2018-05-05 16:54:49 -0400

    commit

--- blog.js
+++ blog.js
@@ -2,15 +2,15 @@
 /*
  * Open the drawer when the menu icon is clicked.
  */
-var tinyNavBar = document.querySelector('#tinyNavBar');
-var leftNavOffScreen = document.querySelector('#leftNavOffScreen');
-var container = document.querySelector('#container');
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
 
-tinyNavBar.addEventListener('click', function (e) {
-    container.classList.toggle('open');
+menu.addEventListener('click', function (e) {
+    drawer.classList.toggle('open');
     e.stopPropagation();
 });
-leftNavOffScreen.addEventListener('click', function () {
-    container.classList.remove('open');
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
 });
 
