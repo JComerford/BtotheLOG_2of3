27352ea63c273329bc2d67438dba7a7e84437833
JComerford <comerford.jac@gmail.com>
2018-05-05 18:20:52 -0400

    commit

--- blog.js
+++ blog.js
@@ -3,14 +3,14 @@
  * Open the drawer when the menu icon is clicked.
  */
 var menu = document.querySelector('#menu');
-var main = document.querySelector('main');
+var dropdown = document.querySelector('.dropdown');
 var drawer = document.querySelector('#drawer');
 
 menu.addEventListener('click', function (e) {
     drawer.classList.toggle('open');
     e.stopPropagation();
 });
-main.addEventListener('click', function () {
+menu.addEventListener('click', function () {
     drawer.classList.remove('open');
 });
 
